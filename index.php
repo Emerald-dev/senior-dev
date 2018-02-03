b10b359d06b3804ecbab3a4c6d27b31fbc00aba2
Brianna Buttaccio <brianna.buttaccio@eagledream.com>
2018-02-03 15:07:21 -0500

    move map code to home page

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 ?>
 
 <h1>Home</h1>
+<?php
+			require_once("assets/php/pinXML.php");
+		?>
+		<div id="map"></div>
+		<script src="assets/js/loadPinMap.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlJM57hL-l7y1cu1B94q8y4nTfOrkbgTI&callback=initMap" async defer></script>
 
 <?php
 	include 'assets/php/footer.php';
