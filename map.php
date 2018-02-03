20cca614e81ce397a4e412f07bfa6722022358f5
Brianna Buttaccio <brianna.buttaccio@eagledream.com>
2018-02-03 12:06:54 -0500

    attempt 5 to fix error on map.php

--- map.php
+++ map.php
@@ -1,26 +1,24 @@
 <html>
 	<head>
-		<title>Simple Map</title>
+		<title>Simple Map<title>
 		<meta name="viewport" content="initial-scale=1.0" charset="utf-8" />
 		<style>
 			#map {
 				height: 100%;
 			}
-			<!-- Optional: Makes the sample page fill the window. -->
 			html, body {
 				height: 100%;
 				margin: 0;
 				padding: 0;
 			}
-		</style>
+		<style>
 	</head>
 	<body>
 		<?php
 			require_once("pinXML.php");
 		?>
 		<div id="map"></div>
-			<script src="loadPinMap.js"></script>
-			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlJM57hL-l7y1cu1B94q8y4nTfOrkbgTI&callback=initMap"
-				async defer></script>
+		<script src="loadPinMap.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlJM57hL-l7y1cu1B94q8y4nTfOrkbgTI&callback=initMap" async defer></script>
 	</body>
 </html>
