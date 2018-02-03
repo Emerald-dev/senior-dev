b39da15e1e2f96f8b446a6e34e7d2560cda38cd9
Claire Noble <csn6973@g.rit.edu>
2018-02-02 23:22:39 -0500

    created xml file to add markers to map

--- map.php
+++ map.php
@@ -18,7 +18,35 @@
 </head>
 <body>
 <div id="map"></div>
-<script>
+<?php
+require_once("dbapi.php");
+    //Create XML file
+    $dom = new DOMDocument("1.0");
+    $node = $dom->createElement("pin");
+    $parnode = $dom->appendChild($node);
+
+    //
+    header("Content-type: text/xml");
+
+    // Iterate through the rows, adding XML nodes for each
+
+    while ($row = getPinData()){
+        // Add to XML document node
+        $node = $dom->createElement("pin");
+        $newnode = $parnode->appendChild($node);
+        $newnode->setAttribute("lat",$row['lat']);
+        $newnode->setAttribute("lon",$row['lon']);
+        $newnode->setAttribute("filters", $row['filters']);
+        $newnode->setAttribute("image", $row['image']);
+        $newnode->setAttribute("name", $row['name']);
+        $newnode->setAttribute("summary", $row['summary']);
+        $newnode->setAttribute("content", $row['content']);
+    }
+
+    echo $dom->saveXML();
+
+?>
+<script type="text/javascript">
     var map,currLocation;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
