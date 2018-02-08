3a2c87fa60f67f632a3b634df96602fcfc6b2e1f
Brianna Buttaccio <Brianna.Buttaccio@ad.eagledream.com>
2018-02-08 12:46:57 -0500

    fix sql schema and insert values for passwords

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -9,7 +9,7 @@ drop table if exists users;
 create table if not exists users(
    id integer not null primary key auto_increment,
    username varchar(20) not null,
-   password varchar(30) not null,
+   password varchar(64) not null,
    email varchar(40) not null,
    salt varchar(10) not null,
    createPrivilege boolean DEFAULT FALSE,
@@ -37,7 +37,7 @@ create table if not exists content(
     id integer not null primary key auto_increment,
     page ENUM('Home', 'Tombstones_&_Natural_History', 'Nearby_Historical_Trails', 'About_Us') not null,
 	dataType ENUM('Large_Title', 'Medium_Title', 'Small_Title', 'Link', 'Image', 'Paragraph') not null,
-    text text  not null
+    text longtext  not null
 )engine=innodb;
 
 
