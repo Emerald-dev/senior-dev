e7f4ec19c7ddf0bbdf903157febbc4a241aac2c1
faridabaks <35696449+faridabaks@users.noreply.github.com>
2018-02-09 20:40:01 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,26 @@
 	include 'assets/php/header.php';
 ?>
 
-<h1>Home</h1>
+
 <?php
 			require_once("assets/php/pinXML.php");
 		?>
 		<div id="map"></div>
 		<script src="assets/js/loadPinMap.js"></script>
 		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlJM57hL-l7y1cu1B94q8y4nTfOrkbgTI&callback=initMap" async defer></script>
+<h1>Rapids Cemetery</h1>
+<h2>Rochester, New York</h2>
+<img src="https://assets.entrepreneur.com/content/3x2/1300/20150406145944-dos-donts-taking-perfect-linkedin-profile-picture-selfie-mobile-camera-2.jpeg" />
+<p>This cemetery is located on the north side of Congress Avenue on the west side of the City of Rochester, N. Y.</p>
+<p>This cemetery was probably founded between 1810 and 1812. The property was originally owned by the Wadsworth family which owned land from Geneseo to Rochester. The Wadsworths set aside one and a quarter acre for a burial place of area residents. The cemetery resided in the Town of Gates until 1902 when the area was annexed into the City of Rochester. The road leading to the cemetery was originally called Cemetery Road. Then between 1880 and 1890 the name was changed to Chester Street. In 1899, Chester Street became Congress Avenue.</p>
+<p>On 20 September 1883 Herbert and William Wadsworth sold the cemetery lot to the newly formed "Rapids Cemetery Association." Monroe H. Oakley was the first president of the organization and Isaac Loomis was treasurer. The last known record of the Rapids Cemetery Association was in 1900 when they paid property taxes. The map, below, was traced from one probably made in the 1890s, although the exact date is unknown. It also included a list of plot owner, which relate to the map.</p>
+<p>Almost all the tombstones have been knocked over. The listing of tombstone inscriptions below was made from old transcriptions and a visit to the cemetery in the late 1980s.</p>
+<h2>Rochester's 'forgotten' land: the Rapids Cemetery</h2>
+<p>This is the story of a piece of land that nobody wanted, a little patch of green in the heart of Rochester\'s west side that may have tremendous significance in this Bicentennial year, but which is now little more than a refuse heap.</p>
+<p>'It is called the "Rapids Cemetery." Located at 65 Congress Avenue, it is older than the City of Rochester itself. But those few people who know what and where it is are lost among the hundreds of cars, pedestrians, and neighborhood kids who pass it every day, totally unaware.</p>
+<p>To most, it is just a brief gap of sunlight in the middle of a shady westside block</p>
+
+
 
 <?php
 	include 'assets/php/footer.php';
