75f1b5ee93a6dea029d2e8a905ed906ac8e9b8e5
KevinsterX <36283983+KevinsterX@users.noreply.github.com>
2018-02-16 09:32:01 -0500

    Add CSS, header, banner.jpg, tombstones and index
    Make changes in those files

--- tombstones.php
+++ tombstones.php
@@ -1,13 +1,16 @@
  <?php
 	$page = 'tombstones';
+	$name = 'Tombstones_&_Natural_History';
 	include 'assets/php/header.php';
 ?>
 
-<h1>Tombstones & Natural History</h1>
+
+
+<!-- <div id="frame"></id> -->
 
 <?php
-	require_once("assets/php/dbapi.php");
+	require_once("assets/php/dbAPI.php");
 	require_once("assets/php/fillPageContent.php");
-	
+
 	include 'assets/php/footer.php';
 ?>
