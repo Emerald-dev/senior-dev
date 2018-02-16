956e2bd47fdf50544e1b1f5b438d0b4b34168c3b
KevinsterX <36283983+KevinsterX@users.noreply.github.com>
2018-02-16 09:09:03 -0500

    Add files via upload

--- about.php
+++ about.php
@@ -1,10 +1,11 @@
  <?php
 	$page = 'about';
+	$name = 'About_Us';
 	include 'assets/php/header.php';
 ?>
 
-<h1>About Us</h1>
-
 <?php
+	require_once("assets/php/dbAPI.php");
+	require_once("assets/php/fillPageContent.php");
 	include 'assets/php/footer.php';
 ?>
