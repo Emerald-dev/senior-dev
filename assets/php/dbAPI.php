505e447f4226656e880a2f8c1047403c9843395d
faridabaks <35696449+faridabaks@users.noreply.github.com>
2018-03-02 22:24:17 +0400

    Update dbAPI.php

--- assets/php/dbAPI.php
+++ assets/php/dbAPI.php
@@ -1,24 +1,24 @@
 <?php
-
+//getting the data out of your tables
 require_once('dbessential.php');
-
+//selecting data from pins table
 function getPinData()
 {
    $sql = "SELECT * FROM pins;";
    return  performActionOnDB($sql);
 
 }
-
+//selecting the data from users table and accepting the variable $username
 function getUser($username){
    $sql = 'SELECT * FROM users WHERE username = "' . $username . '"';
    return  performActionOnDB($sql);
 }
-
+//selecting the data from the content table and accepting the variable $page
  function getContent($page){
    $sql = 'SELECT * FROM content WHERE page = "' . $page . '"';
    return  performActionOnDB($sql);
 }
-
+//selecting the data for text from the content table and accepting the variable $page 
  function getPageTitle($page){
    $sql = 'SELECT text FROM content WHERE page = "' . $page . '" AND dataType = "Large_Title"';
    return  performActionOnDB($sql);
