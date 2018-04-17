4148b4b086c87347d9d2c5eff6e3284bf162892b
bab7607@rit.edu <Brianna.Buttaccio@ad.eagledream.com>
2018-04-17 11:27:07 -0400

    fix typo in singleObjectEdit

--- admin/singleObjectEdit.php
+++ admin/singleObjectEdit.php
@@ -92,7 +92,7 @@ echo("<form action='./singleConfirm.php' method='post'>");
             else if($field == "createPrivilege")
             {
                 echo("$field </br>");
-                echo("<input type='radio' name='$field' value=0 maxlength='$maxlegnth' required> No" );
+                echo("<input type='radio' name='$field' value=0 maxlength='$maxlength' required> No" );
                 echo("<input type='radio' name='$field' value=1 maxlength='$maxlength' required> Yes");
                 echo("<br/>");
             }
